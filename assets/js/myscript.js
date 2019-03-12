130044a1b09490e4154bd1e51ee59aa1872b66b1
mati <jmsalinas@uc.cl>
2019-03-11 22:54:56 -0300

    update contact

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -49,7 +49,7 @@
 	var sectionOne = $('.brief__section-bottom').offset();
 	var sectionTwo = $('.socios').offset();
 	var sectionThree = $('.marcas__section-bottom').offset();
-	var section4 = $('.bottom__section').offset();
+	var sectionFour = $('#bottom').offset();
 
 	const sm = window.matchMedia( "(max-width: 992px)" );
 	const md = window.matchMedia( "(min-width: 992px)" );
@@ -82,26 +82,53 @@
 		$(window).scroll(function() {
 
 			if($(window).scrollTop() + 200 >= sectionOne.top) {
-				$('.menu__full-list-link').addClass('menu__full-list-link-black');
+				$('.menu__full-list-link').addClass('menu__full-list-link-grey');
 				$('.menu__full-list-img-white').css('display', 'none');
 				$('.menu__full-list-img-black').css('display', 'inline');
+				$('.menu__full-list').children('li:nth-child(3)').children('a').children('h4').css('font-weight', '400');
+				$('.menu__full-list').children('li:nth-child(4)').children('a').children('h4').addClass('menu__full-list-link-select-black');
+				$('.menu__full-list').children('li:nth-child(5)').children('a').children('h4').css('font-weight', '400');
 			} else {
-				$('.menu__full-list-link').removeClass('menu__full-list-link-black');
+				$('.menu__full-list-link').removeClass('menu__full-list-link-grey');
 				$('.menu__full-list-img-white').css('display', 'inline');
 				$('.menu__full-list-img-black').css('display', 'none');
+				$('.menu__full-list').children('li:nth-child(3)').children('a').children('h4').css('font-weight', '600');
+				$('.menu__full-list').children('li:nth-child(4)').children('a').children('h4').removeClass('menu__full-list-link-select-black');
 			}
 
-			if ( $(window).scrollTop() >= sectionTwo.top ) {
-				$('.menu__full-list-link').removeClass('menu__full-list-link-black');
+			if ( $(window).scrollTop() + 200 >= sectionTwo.top ) {
+				$('.menu__full-list-link').removeClass('menu__full-list-link-grey');
 				$('.menu__full-list-img-white').css('display', 'inline');
 				$('.menu__full-list-img-black').css('display', 'none');
+				$('.menu__full-list').children('li:nth-child(4)').children('a').children('h4').removeClass('menu__full-list-link-select-black');
+				$('.menu__full-list').children('li:nth-child(5)').children('a').children('h4').css('font-weight', '600');
+				$('.menu__full-list').children('li:nth-child(6)').children('a').children('h4').removeClass('menu__full-list-link-select-black');
 			}
 
 			if ( $(window).scrollTop() >= sectionThree.top ) {
-				$('.menu__full-list-link').addClass('menu__full-list-link-black');
+				$('.menu__full-list-link').addClass('menu__full-list-link-grey');
 				$('.menu__full-list-img-white').css('display', 'none');
 				$('.menu__full-list-img-black').css('display', 'inline');
+				$('.menu__full-list').children('li:nth-child(5)').children('a').children('h4').css('font-weight', '400');
+				$('.menu__full-list').children('li:nth-child(6)').children('a').children('h4').addClass('menu__full-list-link-select-black');
+				$('.menu__full-list').children('li:nth-child(7)').children('a').children('h4').removeClass('menu__full-list-link-select-black');
+			}
+
+			if ( $(window).scrollTop() >= sectionFour.top ) {
+				$('.menu__full-list-link').addClass('menu__full-list-link-grey');
+				$('.menu__full-list-img-white').css('display', 'none');
+				$('.menu__full-list-img-black').css('display', 'inline');
+				$('.menu__full-list').children('li:nth-child(6)').children('a').children('h4').removeClass('menu__full-list-link-select-black');
+				$('.menu__full-list').children('li:nth-child(7)').children('a').children('h4').addClass('menu__full-list-link-select-black');
 			}
 
 		})
 	}
+
+
+// CONTACT BUTTON
+
+	$('.contact_button').on('click', function() {
+		$('.contact_block-top').toggleClass('contact_block-show');
+		$('.contact_block-bottom').toggleClass('contact_block-show');
+	})
